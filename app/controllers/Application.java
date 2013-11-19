be7b1c01c49e4656b88a41d252afac1cc68f0c2e
Markus Wolf <markus.wolfsburg@gmail.com>
2013-11-19 21:36:33 +0100

    sth

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -20,9 +20,24 @@ package controllers;
 
 
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+import javax.ws.rs.core.MediaType;
+
 import play.mvc.Controller;
 import play.mvc.Result;
-
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
+import play.data.Form;
+import views.html.*;
+import de.phoenix.rs.entity.PhoenixTask;
 
 /**
  *  This class handles all the functionality used by the frontend.
@@ -35,8 +50,37 @@ public class Application extends Controller {
      * Displays the home page
      * @return play.mvc.Results.Status
      */
+    
+    private final static String BASE_URI = "http://meldanor.dyndns.org:8080/PhoenixWebService/rest/";
+    
     public static Result home() {
-        return ok("Home");
+        return ok(home.render("Home"));
+    }
+    
+    public static Result createTask() {
+        return ok(createTask.render("Create Task"));
+    }
+    
+    public static Result sendTask() {
+
+        MultipartFormData form = request().body().asMultipartFormData();
+        ArrayList<File> fileLst = new ArrayList<File>();
+        for (FilePart name : form.getFiles())
+            fileLst.add(name.getFile());
+
+        Client c = Client.create();
+        WebResource wr = c.resource(BASE_URI).path(PhoenixTask.WEB_RESOURCE_ROOT).path(PhoenixTask.WEB_RESOURCE_CREATE);
+        PhoenixTask task = null;
+        try {
+            task = new PhoenixTask(Form.form().bindFromRequest().get("title"), Form.form().bindFromRequest().get("description"), fileLst, new ArrayList());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        ClientResponse post = wr.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, task);
+        System.out.println(post.getStatus());
+
+        return ok();
     }
 
 }
