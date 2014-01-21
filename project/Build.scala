3d0da92774a98f54b8df4ab1bb9e3fb4df4299cc
Markus Wolf <markus.wolfsburg@gmail.com>
2014-01-22 00:10:14 +0100

    add TaskSheet

--- project/Build.scala
+++ project/Build.scala
@@ -9,15 +9,15 @@ object ApplicationBuild extends Build {
 
   val appDependencies = Seq(
     // Add your project dependencies here,
+	"de.phoenix" % "library" % "0.0.1-SNAPSHOT",
     javaCore,
     javaJdbc,
     javaEbean,
 	"com.sun.jersey" % "jersey-bundle" % "1.17.1",
-	"de.phoenix" % "library" % "0.0.1-SNAPSHOT",
 	"asm" % "asm" % "3.3.1",
 	"commons-codec" % "commons-codec" % "1.7",
-	"com.fasterxml.jackson.jaxrs" % "jackson-jaxrs-json-provider" % "2.2.1",
-	"com.fasterxml.jackson.datatype" % "jackson-datatype-joda" % "2.2.1",
+	"com.fasterxml.jackson.jaxrs" % "jackson-jaxrs-json-provider" % "2.3.1",
+	"com.fasterxml.jackson.datatype" % "jackson-datatype-joda" % "2.3.1",
 	"joda-time" % "joda-time" % "2.3"
   )
 
