36683a83ba24b3b5c45aa1af8d8e3f813e29e1a5
DrMaggus <markus.wolfsburg@gmail.com>
2014-06-04 12:32:27 +0200

    almost ready! Everything concerning TaskSheets works!

--- app/controllers/TaskSheetApplication.java
+++ app/controllers/TaskSheetApplication.java
@@ -21,7 +21,7 @@ import views.html.addTaskToTaskSheet;
 public class TaskSheetApplication extends Controller {
     
     public static Result showTaskSheets() {
-        return ok(showTaskSheet.render("Show Task Sheets", Requester.TaskSheet.getAll()));
+        return ok(bootstrap.html.showTaskSheets.render("Aufgabenblätter-Pool", Requester.TaskSheet.getAll()));
     }
    
     public static Result createTaskSheet() {
@@ -53,8 +53,10 @@ public class TaskSheetApplication extends Controller {
             String taskSheetTitle = request().queryString().get("tasksheet")[0];
             String taskTitle = request().queryString().get("task")[0];
             Requester.TaskSheet.deleteTask(taskTitle, taskSheetTitle);
-            if (Requester.TaskSheet.getStatus() == 200) 
-                return redirect("/showTaskSheet");
+            if (Requester.TaskSheet.getStatus() == 200) {
+                flash("success",taskSheetTitle);
+                return redirect("/showTaskSheets");
+            }
             else
                 return util.Err.displayError(Requester.TaskSheet.getStatus(),"An error occured while deleting the task.");
         } catch (Exception e) {
@@ -66,15 +68,30 @@ public class TaskSheetApplication extends Controller {
         try {
             String taskSheetTitle = util.Http.GET("tasksheet");
             PhoenixTaskSheet sheet = Requester.TaskSheet.get(taskSheetTitle); 
-            List<PhoenixTask> tasks = Requester.Task.getAll(); 
-            if (Requester.TaskSheet.getStatus() == 200 && Requester.Task.getStatus() == 200)
-                return ok(addTaskToTaskSheet.render("Add Task", sheet, tasks));
+            List<PhoenixTask> alltasks = Requester.Task.getAll(); 
+            ArrayList<PhoenixTask> tasks = new ArrayList<PhoenixTask>();
+             
+            //remove tasks of sheet from choice 
+            boolean add;
+            if (Requester.TaskSheet.getStatus() == 200 && Requester.Task.getStatus() == 200 && taskSheetTitle != null) {
+                for(PhoenixTask t : alltasks) {
+                    add = true;
+                    for(PhoenixTask ts : sheet.getTasks())
+                        if (t.getTitle().equals(ts.getTitle())) 
+                            add = false;
+                    if(add)
+                        tasks.add(t);
+                }
+                                
+                return ok(bootstrap.html.addTaskToTaskSheet.render("Aufgabe hinzufügen", sheet, tasks));
+            }
             else if (Requester.TaskSheet.getStatus() != 200)
                 return util.Err.displayError(Requester.TaskSheet.getStatus(),"Fehler beim holen des Tasksheets!");
             else
                 return util.Err.displayError(Requester.Task.getStatus(),"Fehler beim holen der Tasks!");
         }
         catch (Exception e) {
+            e.printStackTrace();
             return util.Err.displayError(400,"Bad request! Maybe malformed url.");
         }
     }
@@ -85,7 +102,7 @@ public class TaskSheetApplication extends Controller {
         String tasksheet = data.get("tasksheet");
         data.remove("tasksheet");
         data.remove("submit");
-        
+
         ArrayList<String> tasks = new ArrayList<String>();
         
         if (tasksheet != null) 
@@ -94,8 +111,10 @@ public class TaskSheetApplication extends Controller {
             
         Requester.TaskSheet.addTask(tasksheet, tasks);
         
-        if (Requester.TaskSheet.getStatus() == 200)
-            return redirect("/showTaskSheet");
+        if (Requester.TaskSheet.getStatus() == 200) {
+            flash("success",tasksheet);
+            return redirect("/showTaskSheets");
+        }
         else 
             return util.Err.displayError(Requester.TaskSheet.getStatus(),"Fehler beim Senden der Tasks!");
 
